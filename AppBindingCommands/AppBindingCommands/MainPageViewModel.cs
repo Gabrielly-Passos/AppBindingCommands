92c590c3efee7fbef4c07334cb9cb487c2d67080
Gabrielly-Passos <passosgabrielly03@gmail.com>
2022-08-09 21:19:53 -0300

    [UPDATE] MainPageViewModel

---
+++ AppBindingCommands/AppBindingCommands/MainPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace AppBindingCommands
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
